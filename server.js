d90eb7ee9c0dcbf8eb1085629973700645f0e13f
fahimehsd <fahimehsadeghi3@gmail.com>
2023-05-21 14:22:56 +0330

    regiteration done

--- server.js
+++ server.js
@@ -1,20 +1,35 @@
 const express = require("express");
-
 const app = express();
+const bcrypt = require("bcrypt");
 
-const posts = [
-  {
-    username: "Fahimeh",
-    title: "Post 1"
-  },
-  {
-    username: "Faezeh",
-    title: "Post 2"
+const users = [];
+
+app.set("view-engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
+app.get("/", (req, res) => {
+  res.render("index.ejs", { name: "Fahimeh" });
+});
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
+});
+app.post("/login", (req, res) => {});
+
+app.get("/register", (req, res) => {
+  res.render("register.ejs");
+});
+app.post("/register", async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    users.push({
+      id: Date.now(),
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword
+    });
+    res.redirect("/login");
+  } catch (error) {
+    res.redirect("/register");
   }
-];
-
-app.get("/posts", (req, res) => {
-  res.json(posts);
 });
 
 app.listen(3000);
